0b796597a16c067770b749ac0b392f41df0ec838
TheMich157 <142160132+TheMich157@users.noreply.github.com>
2025-12-17 17:00:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
   apiKey: (() => {
-            const apiKey = process.env.API_KEY ||'key_1765984367039_235bc654';
+            const apiKey = process.env.API_KEY ||'mh_p_N47-Bn81bYmnEdNndSdb9rpZFDcrDYzf_6e85';
